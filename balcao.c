c4a0121068862dd75ae778d16526dd1176c8566d
Diogo Pascoal <diogopascoal789@gmail.com>
2021-11-18 15:55:14 +0000

    pipes working

--- balcao.c
+++ balcao.c
@@ -73,23 +73,6 @@ int main(int argc, char*argv[]) {
 
     struct Balcao balcao; //estrutura que guarda a informação necessária ao balcão
 
-    //fork stuff
-    int id, p;
-    int balcao_to_classificador[2];
-    int classificador_to_balcao[2];
-
-    pid_t pidChild;
-
-    int rd;
-    int rdup;
-    int wdup;
-    char *eol;
-
-    //comunicação com o Classificador
-
-    char descricaoProblema[50];
-    char resposta[50];
-
 
     //inicialização
     if( obtemVariaveisAmbiente(&balcao) == -1)
@@ -97,22 +80,66 @@ int main(int argc, char*argv[]) {
     inicializaStruct(&balcao);
 
 
-    //criar pipe balcao -> classificador
-    if(pipe(balcao_to_classificador) < 0){
-        fprintf(stderr,"Erro ao criar pipe balcao -> classificador");
-        return -1;
+    int fd_balcao_classificador[2],fd_classificador_balcao[2];
+
+    if(pipe(fd_balcao_classificador) == -1 || pipe(fd_classificador_balcao) == -1){
+        fprintf(stderr,"\nErro - Nao foi possivel criar pipes\n");
+        exit(1);
     }
 
-    if(pipe(classificador_to_balcao) < 0){
-        fprintf(stderr,"Erro ao cirar pipe classificador -> balcao");
-        return -1;
+    switch (fork())
+    {
+    case -1: //error fork
+        fprintf(stderr,"\nErro - Nao foi possivel criar fork\n");
+        exit(2);
+        break;
+    case 0:{ // child - run classificador
+
+    // write 1 -> 0 read
+        close(STDIN_FILENO);
+        dup(fd_balcao_classificador[0]);
+        close(fd_balcao_classificador[0]);
+        close(fd_balcao_classificador[1]);
+
+
+        close(STDOUT_FILENO);
+        dup(fd_classificador_balcao[1]);
+        close(fd_classificador_balcao[1]);
+        close(fd_classificador_balcao[0]);
+
+        execl("classificador","classificador",NULL);
+
+        break;
+
     }
+    default:{//parent
 
-    switch(pidChild = fork()){
-        
+        char input[100];
+        char output[100];
+
+        close(fd_balcao_classificador[0]);
+        close(fd_classificador_balcao[1]);
+
+        while(1){
+            printf("\nIndique os sintomas: ");
+            fgets(input,99,stdin);
+
+            write(fd_balcao_classificador[1],input,strlen(input));
+            read(fd_classificador_balcao[0],output,99);
+            fprintf(stdout,"%s",output);
+
+            if (strcmp(input,"#fim\n") == 0)
+                break;
+            
+            //just cleaning memory
+            fflush(stdout);
+            fflush(stdin);
+            memset(output,0,99);
+            memset(input,0,99);
+        }
+        break;
+    }   
     }
-
-
 
 }
 
